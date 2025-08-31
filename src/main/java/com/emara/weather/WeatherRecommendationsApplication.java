6405bf471acdc119dc4e67afb77bab7b70e92b03
Mohamed Emara <medoemara104@gmail.com>
2025-08-31 12:23:39 +0300

    Setup Spring-Boot project, Create database, seed cities to DB

---
+++ src/main/java/com/emara/weather/WeatherRecommendationsApplication.java
@@ -0,0 +1,13 @@
+package com.emara.weather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherRecommendationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherRecommendationsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/emara/weather/WeatherRecommendationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emara.weather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherRecommendationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
